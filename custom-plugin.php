d24e03e86b123001439d1b55a862e83343947aab
Aqsa Mumtaz <codewithaqsa@gmail.com>
2024-12-31 14:13:05 +0500

    added the sub-menu

--- custom-plugin.php
+++ custom-plugin.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-*plugin Name: Custom Plugin
-*Description: This is my first plugin for learning
+*plugin Name: Employee Management system
+*Description: This is CRUD Employee Management system
 *Plugin URI: https://www.example.com/custom-plugin
 *Author: Aqsa Mumtaz
 *Author URI: https://www.example.com
@@ -12,13 +12,31 @@
 
 */
 
+//action hook to add menu
 add_action("admin_menu", "cp_add_admin_menu");
 
-
+//add menu
 function cp_add_admin_menu(){
+add_menu_page("Employee System | Employee Management System", "Employee System", "manage_options", "employee-system",
+ "ems_crud_system", "dashicons-groups", 23);
 
-add_menu_page("Custom Plugin Menu", "Custom Plugin", "manage_options", "cp-plugin" , "cp_handle_admin_menu", );
 
+//sub-menu
+add_submenu_page("employee-system", "Add Employee", "Add Employee" , "manage_options", "ems_add_employee", "ems_crud_system" );
+
+add_submenu_page("employee-system", "List Employee", "List Employee", "manage_options" , "list-employee", "ems_list_employee");
+}
+
+
+//menu handle callback
+function ems_crud_system(){
+
+    echo "<h2>Well Come to my Employee Management System</h2>";
+}
+
+//submenu call back function
+function ems_list_employee(){
+    echo "<h2>Well Come to the List of Employee</h2>";
 }
 
 ?>
